608c9e8e3289919eb86f9ede7410f2479c263d2f
asalek <asalek8599@gmail.com>
2022-05-20 20:30:25 +0100

    update: syntax simplifying

--- check_path.c
+++ check_path.c
@@ -1,52 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_path.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalek <asalek@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 20:28:41 by asalek            #+#    #+#             */
+/*   Updated: 2022/05/20 20:28:42 by asalek           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-char **split_path(char *string)
+char	**split_path(char *string)
 {
-    char **path;
+	char	**path;
 
-    if (string == NULL)
-        return NULL;
-    
-    path = ft_split(string,':');
-    return path;
+	if (string == NULL)
+		return (NULL);
+	path = ft_split(string, ':');
+	return (path);
 }
 
-char *return_path(char **envp)
+char	*return_path(char **envp)
 {
-    int i;
-    char *path;
+	int		i;
+	char	*path;
 
-    i = 0;
-    while (envp[i])
-    {
-        if(ft_strnstr(envp[i],"PATH=",ft_strlen(envp[i])) != 0)// in mac PATH=/usr
-            path = ft_substr(envp[i],5,ft_strlen(envp[i]));// in mac PATH=/usr
-        i++;
-    }
+	i = 0;
+	while (envp[i])
+	{
+		if (ft_strnstr(envp[i], "PATH=", ft_strlen(envp[i])) != 0)
+			path = ft_substr(envp[i], 5, ft_strlen(envp[i]));
+		i++;
+	}
 	if (!path)
 		return (NULL);
-    return path;
+	return (path);
 }
 
-char *concatenate_string(char *original, char *add)
+char	*concatenate_string(char *original, char *add)
 {
-    int i = 0;
-    int j = 0;
+	int		i;
+	int		j;
+	char	*alloc1;
 
-    char *alloc1 = malloc(sizeof(char) *( ft_strlen(original) + ft_strlen(add) + 1));
-    
-    while (original[j])
-    {
-        alloc1[j] = original[j];
-        j++;
-    }
-   
-   while(add[j])
-   {
-      alloc1[j] = add[i];
-      i++;
-      j++;
-   }
-   alloc1[j] = '\0';
-   return alloc1;
+	alloc1 = malloc(sizeof(char) * (ft_strlen(original) + ft_strlen(add) + 1));
+	i = 0;
+	j = 0;
+	while (original[j])
+	{
+		alloc1[j] = original[j];
+		j++;
+	}
+	while (add[j])
+	{
+		alloc1[j] = add[i];
+		i++;
+		j++;
+	}
+	alloc1[j] = '\0';
+	return (alloc1);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: asalek <asalek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 13:28:22 by asalek            #+#    #+#             */
-/*   Updated: 2022/05/20 17:37:03 by asalek           ###   ########.fr       */
+/*   Updated: 2022/05/20 17:49:21 by asalek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,49 +57,48 @@ typedef struct echo_parssing{
 	char	*line;
 }	t_echo;
 
-void		hendl_two(char *line, int i);
-void		hendl_one(char *line, int i);
-void		redairection_1(t_echo *e);
-char		*line_handle(char *line, t_echo *e, t_data *t);
-int			check_if_is_echo(char *line);
-int			check_3(char *line, t_echo *e);
-int			check_2(char *line, t_echo *e, int i, t_data *t);
-int			check_1(char *line, t_echo *e, t_data *t);
-int			redairection_handle(char *line);
-int			check_qouts_echo(char *line, t_echo *e);
-int			hendl_1(char *str, int i);
-int			check_qouts(char *line, t_echo *e);
-int			has_quotes(char *str);
-char		*replace_arg_env(char *str, t_data *t);
-char		*cmd_found(int count, char **split_p, char *cmd);
-void		execut_cmd(char *path, char **cmd, char *command, t_data *t);
-void		init_func(char **envp);
-void		check_quots(char *cmd);
-t_list2		*creat_list(char **str);
-t_list2		*ft_lstneww(char *v_name, char *v_value);
-void		ft_lstadd_backk(t_list2 **lst, t_list2 *new);
-int			more_built_in(char **cmd, t_data *t);
-int			built_in_functions(char **cmd, t_data *t);
-void		handl_line(char *cmd, t_data *t);
-void		Pipe_hundler(t_echo *e, t_data *t);
-void		pipee(t_echo *e, t_data *t);
-void		command_not_found(int i);
-int			count(char **cmd);
-char		**split_path(char *string);
-char		*return_path(char **envp);
-char		*concatenate_string(char *original, char *add);
-size_t		count_words(char const *s, char c);
-void		ft_echo(char **str, t_data *t);
-void		ft_env(t_data *t);
-void		unset(char *arg, t_data *t);
-void		export(char *arg, t_data *t);
-void		cd(char *str, char **envp);
-void		*pwd(void);
-void		ft_exit(void);
-void		exit_shell(int a);
-void		execute_path(t_data *t, char **split_p, char **cmd_split, char *cmd);
-int	check_single_quots(char *line, t_echo *e);
-int	check_double_quots(char *line, t_echo *e);
-// static int	check_single(char *line, t_echo *e);
-// static int	check_double(char *line, t_echo *e);
+void	hendl_two(char *line, int i);
+void	hendl_one(char *line, int i);
+void	redairection_1(t_echo *e);
+char	*line_handle(char *line, t_echo *e, t_data *t);
+int		check_if_is_echo(char *line);
+int		check_3(char *line, t_echo *e);
+int		check_2(char *line, t_echo *e, int i, t_data *t);
+int		check_1(char *line, t_echo *e, t_data *t);
+int		redairection_handle(char *line);
+int		check_qouts_echo(char *line, t_echo *e);
+int		hendl_1(char *str, int i);
+int		check_qouts(char *line, t_echo *e);
+int		has_quotes(char *str);
+char	*replace_arg_env(char *str, t_data *t);
+char	*cmd_found(int count, char **split_p, char *cmd);
+void	execut_cmd(char *path, char **cmd, char *command, t_data *t);
+void	init_func(char **envp);
+void	check_quots(char *cmd);
+t_list2	*creat_list(char **str);
+t_list2	*ft_lstneww(char *v_name, char *v_value);
+void	ft_lstadd_backk(t_list2 **lst, t_list2 *new);
+int		more_built_in(char **cmd, t_data *t);
+int		built_in_functions(char **cmd, t_data *t);
+void	handl_line(char *cmd, t_data *t);
+void	Pipe_hundler(t_echo *e, t_data *t);
+void	pipee(t_echo *e, t_data *t);
+void	command_not_found(int i);
+int		count(char **cmd);
+char	**split_path(char *string);
+char	*return_path(char **envp);
+char	*concatenate_string(char *original, char *add);
+size_t	count_words(char const *s, char c);
+void	ft_echo(char **str, t_data *t);
+void	ft_env(t_data *t);
+void	unset(char *arg, t_data *t);
+void	export(char *arg, t_data *t);
+void	cd(char *str, char **envp);
+void	*pwd(void);
+void	ft_exit(void);
+void	exit_shell(int a);
+void	execute_path(t_data *t, char **split_p, char **cmd_split, char *cmd);
+int		check_single_quots(char *line, t_echo *e);
+int		check_double_quots(char *line, t_echo *e);
+
 #endif

d9dd6a47e4375ea3950402627bac31093cfd440b
Ayoub Salek <asalek@e3r8p8.1337.ma>
2022-06-12 20:26:51 +0100

    update: the syntax according to norm syntax

--- check_pipe.c
+++ check_pipe.c
@@ -1,25 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_path.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asalek <asalek@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/20 20:28:41 by asalek            #+#    #+#             */
+/*   Updated: 2022/05/20 20:28:42 by asalek           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-int check_pipe_inside_quotes(char *str)
+int	check_pipe_inside_quotes(char *str)
 {
-    int     i;
-    int     j;
-    char    c;
+	int		i;
+	int		j;
+	char	c;
 
-    i = 0;
-    j = 0;
-    while (str[i])
-    {
-        if (str[i] == '\'' || str[i] == '"')
-        {
-            c = str[i];
-            i++;
-            while (str[i] != c)
-                i++;
-        }
-        else if (str[i] == '|')
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	j = 0;
+	while (str[i])
+	{
+		if (str[i] == '\'' || str[i] == '"')
+		{
+			c = str[i];
+			i++;
+			while (str[i] != c)
+				i++;
+		}
+		else if (str[i] == '|')
+			return (1);
+		i++;
+	}
+	return (0);
 }
